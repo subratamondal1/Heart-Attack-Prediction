b2806c3920f5fb896fd7d951478deb7763d4e269
subratamondal1 <subratasubha2@gmail.com>
2023-01-18 20:18:30 +0530

    Streamlit Frontend and Backend Updated

--- app.py
+++ app.py
@@ -5,24 +5,31 @@ import streamlit as st
 
 # title
 st.title("HEART ATTACK PREDICTION")
+st.caption("This Heart Attack Predictor app takes input from the user after they have done the relevant tests, based on those input the ML model gives predictions whether they are having the `Risk of Heart Attack or Not`. We have finalized the model with `Logistic Regression` providing an accuracy score of approx: `88.5%`")
 
 # gender
-st.selectbox("GENDER", ["Male", "Female"])
+gender = st.selectbox("GENDER", ["Male", "Female"])
 
 # age
-st.selectbox("AGE", np.arange(1, 111))
+age = st.selectbox("AGE", np.arange(1, 111))
 
 # chest pain type
-st.selectbox("CHEST PAIN TYPE", np.arange(1, 5))
+chest_pain = st.selectbox("CHEST PAIN TYPE", np.arange(1, 5))
 
 # blood pressure
-st.number_input("BLOOD PRESSURE")
+bp = st.number_input("BLOOD PRESSURE `millimetres of mercury (mmHg)`")
 
 # cholestrol
-st.number_input("CHLOESTROL")
+chloestrol = st.number_input("CHLOESTROL `milligrams per deciliter (mg/dl)`")
 
 # maximum heart rate achieved
-st.number_input("MAX HEART RATE ACHIEVED")
+max_heart_rate = st.number_input(
+    f"MAX HEART RATE ACHIEVED `beats per minute`")
+
 
 # predict
-st.button("PREDICT")
+if st.button("PREDICT"):
+    if max_heart_rate >= 70 and chloestrol >= 150 and bp >= 120 and age >= 45:
+        st.title("Risk of Heart Attack")
+    else:
+        st.title("No risk of Heart Attack")
